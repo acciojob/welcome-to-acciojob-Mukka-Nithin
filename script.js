44869fd44d1215f4d1305c73f4d8e7ebc434b282
Mukka Nithin <mukkanithin540@gmail.com>
2025-12-19 20:06:34 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
-//your JS code here. If required.
+// JavaScript for basic interactivity
+
+const button = document.getElementById("learnMoreBtn");
+
+button.addEventListener("click", function () {
+    alert("Welcome to AccioJob! Start learning and build your career 🚀");
+});
+
